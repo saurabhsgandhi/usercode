0f1fced4200c967c861cf01c721874030800d121
A154499 <saurabh.gandhi@Bestbuy.com>
2025-12-19 01:15:13 -0600

    1

--- stock-app/src/components/Layout.js
+++ stock-app/src/components/Layout.js
@@ -1,1 +1,17 @@
-// Write your Layout component here
+import React from 'react';
+import Header from '../components/Header';
+import Slider from '../components/Slider';
+import { Outlet  } from "react-router-dom";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Slider />
+      <Header />
+      <main>{children}</main>
+      <Outlet />
+    </>
+  );
+}
+
+export default Layout;
--- stock-app/src/components/SingleCard.js
+++ stock-app/src/components/SingleCard.js
@@ -1,1 +1,17 @@
-// Write your Single Card component here
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Card = ({ symbol }) => {
+    const navigate = useNavigate();
+
+    return (
+      <div className="card">
+        <div className="card__body">
+          <h2 className="card__title">{symbol}</h2>
+        </div>
+        <button className="card__btn" onClick={() => navigate(`/stocks/:${symbol}`, { state: symbol})}>View Details</button>
+      </div>
+    );
+}
+
+export default Card;
